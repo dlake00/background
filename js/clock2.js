a7e6470e2b5b439eae0f668ea5d66ffda4eb3614
lake <daniel.lake@medway.gov.uk>
2020-08-10 14:42:10 +0100

    Added clock / countup timer

---
+++ js/clock2.js
@@ -0,0 +1,21 @@
+var timerVar = setInterval(countTimer, 1000);
+var totalSeconds = 0;
+
+function countTimer() {
+  ++totalSeconds;
+
+  var hour = Math.floor(totalSeconds /3600);
+  var minute = Math.floor((totalSeconds - hour*3600)/60);
+  var seconds = totalSeconds - (hour*3600 + minute*60);
+
+  if(hour < 10) {
+    hour = "0" + hour;
+  }
+  if(minute < 10) {
+    minute = "0" + minute;
+  }
+  if(seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  document.getElementById("clock2").innerText = hour + ":" + minute + ":" + seconds;
+}
